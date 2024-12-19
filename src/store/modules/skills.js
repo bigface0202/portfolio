3bd473cebd19465485bff0e12644a9f399386038
e.yusuke <theshoreunder@gmail.com>
2024-12-19 22:44:57 +0900

    Fix year

--- src/store/modules/skills.js
+++ src/store/modules/skills.js
@@ -4,17 +4,17 @@ export default {
     skills: [
       {
         title: 'Google Cloud',
-        years: '2.5 years',
+        years: '3 years',
         icon: 'mdi-google-cloud'
       },
       {
         title: 'Kubernetes',
-        years: '2.5 years',
+        years: '2 years',
         icon: 'mdi-kubernetes'
       },
       {
         title: 'Terraform',
-        years: '2.5 years',
+        years: '3 years',
         icon: 'mdi-terraform'
       },
       {
@@ -29,18 +29,18 @@ export default {
       },
       {
         title: 'C++',
-        years: '2 year',
+        years: '2 years',
         icon: 'mdi-language-cpp'
       },
       {
         title: 'Vue.js',
-        years: '2 year',
+        years: '2 years',
         icon: 'mdi-vuejs'
       }
     ]
   }),
   getters: {
-    skills (state) {
+    skills(state) {
       return state.skills
     }
   }
